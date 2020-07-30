c6a6ffa1687fc938a7d444698dde5e9e084f4bb1
freddymi1 <michelfreddy1992@gmail.com>
2020-07-30 12:04:35 +0300

    Commit de site de Fy-deco sur github

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.css">
+    <script src="assets/js/scrollreveal.js"></script>
+    <title>F'Y DECO</title>
+</head>
+<body>
+    <header>
+        <nav class="nav">
+            <div class="menu-toggle">
+                <i class="fa fa-bars"></i>
+                <i class="fa fa-times"></i>
+            </div>
+            <a href="index.php" class="logo"><img src="assets/imgs/logo/logo-fy.png" alt=""></a>
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link active">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#about" class="nav-link">Apropos</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#service" class="nav-link">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#reservation" class="nav-link">Reservation</a>
+                </li>
+                <!--<li class="nav-item">
+                    <a href="#" class="nav-link">Nouveautés</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Magasin</a>
+                </li>-->
+                <li class="nav-item">
+                    <a href="#contact" class="nav-link">contact</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="container">
+            
+        </div>
+    </header>
+    <!-- Fin du Header-->
+
+    <section class="hero" id="hero">
+        <div class="container">
+            <div class="header-containt">
+                <h2 class="sub-headline">
+                    <span class="first-letter">B</span>ienvenue chez
+                </h2>
+                <h1 class="headline">F'Y DECO</h1>
+                <div class="headline-description">
+                    <div class="separator">
+                        <div class="line line-left"></div>
+                        <div class="asterisk"><i class="fa fa-asterisk"></i></div>
+                        <div class="line line-right"></div>
+                    </div>
+                    <div class="single-animation">
+                        <h5>Pret à être ouvert</h5>
+                        <a href="#" class="btn cta-btn">Plus d'information</a>
+                    </div>
+                </div>
+            </div>
+            <!--<div class="back-to-down">
+                <a href="#about"><i class="fa fa-chevron-down"></i></a>
+            </div>-->
+        </div>
+       
+    </section>
+    <!-- Fin du hero-->
+    <section class="discover-our-story" id="about">
+        <div class="container">
+            <div class="immo-info">
+                <div class="immo-description padding-right animate-left">
+                    <div class="global-headline">
+                        <h2 class="sub-headline">
+                            <span class="first-letter">D</span>écouvrir
+                        </h2>
+                        <h1 class="headline headline-blue">Notre Histoire</h1>
+                        <div class="asterisk"><i class="fa fa-asterisk"></i></div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, velit nostrum possimus, tenetur accusantium eius esse omnis praesentium architecto magnam voluptates eos. Magnam ducimus tenetur itaque nesciunt amet possimus illum?
+                    </p>
+                    <a href="#" class="btn body-btn">Plus d'information</a>
+                </div>
+                <div class="immo-info-img animate-right">
+                    <img src="assets/imgs/slider/14.jpg" class="img-about" alt="Notre histoire">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--  Fin du decouvrire notre histoire--->
+    <section class="tasteful-recipes between">
+        <div class="container">
+            <div class="global-headline">
+                <div class="nimate-top">
+                    <h2 class="sub-headline">
+                        <span class="first-letter">C</span>hambre de cuisine
+                    </h2>
+                </div>
+                <div class="animate-bottom">
+                    <h1 class="headline">Bien equipe</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+     <!--  Fin du tasteful recipese--->
+     <section class="discover-our-service" id="service">
+        <div class="container">
+            <div class="immo-info">
+                <div class="image-group padding-right animate-left">
+                   <div class="inner-img">
+                        <a href="assets/imgs/slider/3.jpg" target="blank">
+                            <img src="assets/imgs/slider/3.jpg" alt="">
+                        </a>
+                   </div>
+                    <div class="inner-img">
+                        <a href="assets/imgs/slider/4.jpg" target="blank">
+                            <img src="assets/imgs/slider/4.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="inner-img">
+                        <a href="assets/imgs/slider/8.jpg" target="blank">
+                            <img src="assets/imgs/slider/8.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="inner-img">
+                        <a href="assets/imgs/slider/15.jpg" target="blank">
+                            <img src="assets/imgs/slider/15.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="immo-description animate-right">
+                    <div class="global-headline">
+                        <h2 class="sub-headline">
+                            <span class="first-letter">D</span>écouvrir
+                        </h2>
+                        <h1 class="headline headline-blue">Nos Services</h1>
+                        <div class="asterisk"><i class="fa fa-asterisk"></i></div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita asperiores temporibus ab optio repellat esse accusantium cupiditate enim mollitia ipsum aliquam rerum harum assumenda sint voluptas facere cum, sapiente dolorem!
+                    </p>
+                    <a href="#" class="btn body-btn">Voir tous nos services</a>
+                </div>
+            </div>
+        </div>
+     </section>
+     <!--Fin du découvrire les menu-->
+     <section class="perfect-room between">
+        <div class="container">
+            <div class="global-headline">
+                <div class="nimate-top">
+                    <h2 class="sub-headline">
+                        <span class="first-letter">C</span>hambre
+                    </h2>
+                </div>
+                <div class="animate-bottom">
+                    <h1 class="headline"> de salon</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Fin du perfect blend-->
+    <section class="culiry-delight" id="reservation">
+        <div class="container">
+            <div class="immo-info">
+                <div class="immo-description padding-right animate-left">
+                    <div class="global-headline">
+                        <h2 class="sub-headline">
+                            <span class="first-letter">C</span>ulinary
+                        </h2>
+                        <h1 class="headline headline-blue">Delight</h1>
+                        <div class="asterisk"><i class="fa fa-asterisk"></i></div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, velit nostrum possimus, tenetur accusantium eius esse omnis praesentium architecto magnam voluptates eos. Magnam ducimus tenetur itaque nesciunt amet possimus illum?
+                    </p>
+                    <a href="#" class="btn body-btn">Faite vos réservation</a>
+                </div>
+                <div class="image-group">
+                    <img class="animate-top" src="assets/imgs/slider/11.jpg" alt="">
+                    <img class="animate-bottom" src="assets/imgs/slider/12.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--- Fin du Culinary section -->
+    <section class="contact-us between" id="contact">
+        <div class="container">
+            <div class="immo-info">
+                <div class="contact-group padding-right animate-left">
+                    <form action="" class="form-contact">
+                        <div class="form-group">
+                            <div class="form-control">
+                                <div class="box">
+                                    <div class="np">
+                                        <div class="names">
+                                            <div class="inputBox">
+                                                <input type="text" name="nom" id="nom" class="nom" placeholder="" required/>
+                                                <label>Votre nom</label>
+                                            </div>
+                                        </div>
+                                        <div class="lastanmes">
+                                            <div class="inputBox">
+                                                <input type="text" class="prenoms" id="prenoms" name="prenom" placeholder="" required/>
+                                                <label class="prenoms">Votre prénoms</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="inputBox">
+                                        <input type="email" name="email" class="email" id="email" placeholder="" required/>
+                                        <label>Votre adresse email</label>
+                                    </div>
+                                        
+                                    <div class="inputBox">
+                                        <textarea name="message" id="message" class="message" cols="30" rows="4"></textarea>
+                                        <label>Votre message</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn contact-btn">Contact rapide</button>
+                    </form>
+                    
+                </div>
+                <div class="immo-description animate-right">
+                    <div class="global-headline">
+                        <h2 class="sub-headline">
+                            <span class="first-letter">D</span>ontactez
+                        </h2>
+                        <h1 class="headline headline-blue">Nous</h1>
+                        <div class="asterisk"><i class="fa fa-asterisk"></i></div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita asperiores temporibus ab optio repellat esse accusantium cupiditate enim mollitia ipsum aliquam rerum harum assumenda sint voluptas facere cum, sapiente dolorem!
+                    </p>
+                    <a href="#" class="btn body-btn">Faite vos réservation</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Fin du section contact -->
+    <footer>
+        <div class="container">
+            <div class="back-to-top">
+                <a href="#hero"><i class="fa fa-chevron-up"></i></a>
+            </div>
+            <div class="footer-content">
+                <div class="footer-content-about animate-top">
+                    <div class="footer-logo">
+                        <img src="assets/imgs/logo/logo-fy.png" alt="">
+                    </div>
+                    <h4>Apropos du F'Y DECO</h4>
+                    
+                    <div class="asterisk"><i class="fa fa-asterisk"></i></div>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium debitis sint quidem error qui quasi eius. Possimus quam ad nemo assumenda ex earum? Adipisci quibusdam numquam est, aut in voluptatum? Ipsa atque accusamus iusto rem.
+                    </p>
+                </div>
+                <div class="footer-content-divider animate-bottom">
+                    <div class="social-media">
+                        <h4>Suivez-nous sur:</h4>
+                        <ul class="social-icons">
+                            <li>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-facebook-square"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-pinterest"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-google"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="newsletter-container">
+                        <h4>Information</h4>
+                        <form action="" class="newsletter-form">
+                            <input type="text" placeholder="Votre adresse email" class="newsletter-input">
+                            <button class="newsletter-btn" type="submit">
+                                <i class="fa fa-envelope"></i>    
+                            </button>
+                        </form>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+        <div class="copyright">
+            <h5>&copy F'y deco 2020</h5>
+        </div>
+    </footer>
+    <script src="assets/js/jquery-3.4.1.slim.js"></script>
+    <script src="assets/js/scrollreveal.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
